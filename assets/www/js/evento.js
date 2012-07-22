6c4b0bf74d2cce0ddb164ec084ef5d2303e9fbac
kiko <kiko@kai.(none)>
2012-07-22 03:44:42 +0200

    20_julio_2

---
+++ assets/www/js/evento.js
@@ -0,0 +1,183 @@
+//VARIABLES
+
+var var_nombre;
+var var_description;
+var var_fecha;
+var var_dia;
+var var_mes;
+var var_precio;
+var var_sala;
+var var_location;
+var var_longitude;
+var var_latitude;
+var var_asistentes;
+var var_conf="";
+
+//FUNCIONES
+
+$(document).ready(function() {
+    $("body").css("display", "none");
+    $("body").fadeIn('slow');
+
+    document.addEventListener("deviceready", function() {console.log("PhoneGap initialized.")}, false);    
+});
+    
+function leerGET(){
+    var cadGET = location.search.substr(1,location.search.length);
+	var arrGET = cadGET.split("&");
+	var asocGET = new Array();
+	var variable = "";
+	var valor = "";
+	for(i=0;i<arrGET.length;i++){
+		 var aux = arrGET[i].split("=");
+		 variable = aux[0];
+		 valor = aux[1];
+ 		asocGET[variable] = valor;
+	}
+ return asocGET;
+} 
+
+
+function onBodyLoad() {
+	
+	var paresVarValor = leerGET();
+	User_id = paresVarValor['User'];
+	Evento = paresVarValor['Evento'];
+	
+	if(User_id==undefined){
+		var asistencia= document.getElementById("confirmar");
+		asistencia.onclick=function(){registrate();}
+	}
+
+	obtenerDatos();
+	ComprobarAsistencia();
+	
+} 
+  
+function cambiar(){
+ 
+    if(document.getElementById('confirmar').getAttribute("class") == 'por_confirmar'){
+    	document.getElementById('confirmar').setAttribute("class","confirmado");
+        $('#quantity').val(parseInt($('#quantity').val()) + 1); 	
+        ConfirmarAsistencia(0); 
+    }
+    
+    else{
+    	document.getElementById('confirmar').setAttribute("class","por_confirmar");    	
+        $('#quantity').val(parseInt($('#quantity').val()) - 1);
+        ConfirmarAsistencia(User_id);        	  
+    }
+    
+}
+    
+function obtenerDatos() {
+    
+    $.ajax({
+        url: 'http://192.168.1.102/evento_info.php?evento='+Evento,
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        type:'get',
+        timeout: 5000,
+        success: function(data/*, status*/){
+            $.each(data, function(i,item){
+               var_nombre = item.name;
+               var_description = item.description;
+               
+               var_fecha = item.date_init;  
+               var_dia = item.Day;  
+               var_mes = item.Month;  
+                
+               var_precio = item.price;    
+               var_sala = item.venue;
+               var_location = item.location;
+               var_latitude = item.latitude;
+               var_longitude = item.longitude
+               var_asistentes = item.cuenta;               
+
+            });
+            mostrardatos();
+        },
+        error: function(){
+            alert('no va');
+        }
+    });
+}
+
+function ComprobarAsistencia() {
+    
+    $.ajax({
+        url: 'http://192.168.1.102/bandee_asistencia_evento.php?evento='+Evento+'&user='+user,
+        dataType: 'jsonp',
+        jsonp: 'jsoncallback',
+        type:'get',
+        timeout: 5000,
+        success: function(data/*, status*/){
+                $.each(data, function(i,item){
+                   var_conf = "TRUE";       
+                });
+                
+                if(var_conf=="TRUE")
+                	document.getElementById('confirmar').setAttribute("class","confirmado");
+                	
+                else
+                	document.getElementById('confirmar').setAttribute("class","por_confirmar");         
+                
+            },
+            error: function(){
+
+            }
+        });
+    }
+    
+   function ConfirmarAsistencia(i) {		
+        
+        $.ajax({
+            url: 'http://192.168.1.102/evento_confirmar_asistencia.php?user_id='+User_id+'&event_id='+Evento+'&valor='+i,
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            type:'get',
+            timeout: 5000,
+            success: function(data){
+
+            }, 
+            error: function(){
+
+            }
+        });
+    }
+    
+    function mostrardatos() {
+        $('#EVENTO_mes').html(var_mes);
+        $('#EVENTO_dia').html(var_dia);
+        $('#EVENTO_nombre').html(var_nombre);
+		$('#EVENTO_hora').html(var_fecha);
+		$('#EVENTO_sala').html(var_sala);
+		$('#EVENTO_lugar').html(var_location);
+		$('#EVENTO_descripcion').html(var_description);
+		$('#EVENTO_precio').html(var_precio + ' €');
+		$('#quantity').val(var_asistentes);
+		
+        $("#cover").fadeOut('fast');
+		$("#base").fadeIn('fast');
+    }
+    
+	function zonaprivada(){
+		event.preventDefault();
+    	window.location  = "login.html";
+    	$("body").fadeOut('slow',redirectPage);     
+	}
+	
+	function continuar(){
+    	event.preventDefault();
+    	window.location = "bandas.html";
+    	$("body").fadeOut('slow',redirectPage);     
+	}	
+	
+	function verMapa(){
+		window.location = 'https://maps.google.com/maps?q='+var_latitude+',+'+var_longitude+'&iwloc=A&hl=es';
+	}
+	
+	function registrate(){
+		alert("Debes registrarte para esto, dale caña!");
+	}
+	
--- assets/www/js/index.js
+++ assets/www/js/index.js
@@ -3,47 +3,123 @@
  * and open the template in the editor.
  */
 
-//Variables globales
-//
-var $resultados;
-
-
-function obtenerDatos() {
-    valor=document.getElementById("dato").value;
-    var valor ='paco';
-    alert(valor);
+//VARIABLES
+  var linkLocation;
+ 
+ //FUNCIONES   
+    $(document).ready(function() {
+        $("body").css("display", "none");
+  	    $("body").fadeIn('slow');
     
-    resultados = $('#resultados');
-          
-    $.ajax({
-        url: 'http://158.42.46.225/buscador.php?busqueda='+valor,
-        //url: '../html/bandee.php',
-        //NO SE PUEDE LANZAR A UN LOCALHOST, TIENE QUE SER A UN WEB-SERVER (HTTP)
-        //url: 'http://158.42.143.175/bandee.php', IP ASIGNADA X LA UPV
-        //url: 'http://158.42.141.22/bandee.php',
-        //url: '///android_asset/www/html/bandee.php',
-        dataType: 'jsonp',
-        jsonp: 'jsoncallback',
-        timeout: 5000,
-        success: function(data, status){
-            $.each(data, function(i,item){
-                alert(item.id);
-                //$resultados = item.id;
-                var landmark = '<h1>'+item.id+'</h1>';
-                resultados.append(landmark);
-                //$.mobile.pageLoading(true);
-                mostrardatos();
-            });
-        },
-        error: function(){
-            resultados.text('There was an error loading the data.');
-        }
+        document.addEventListener("deviceready", function() {console.log("PhoneGap initialized.")}, false);    
     });
-}
-		    
-function mostrardatos() {
-    //window.open("https://www.gafasonline.es");
-    alert("Hola2");
-    $('#resultados').html($resultados);
-    $.mobile.pageLoading(true);
-}
+
+    function onBodyLoad() {   	
+		obtenerDatos();
+    }
+    
+	function mostrarEstilos() {
+
+		div = document.getElementById('resul');
+		div2 = document.getElementById('formularioEstilo');
+		div3 = document.getElementById('formularioCiudades');
+		div4 = document.getElementById('formularioCercania');
+		
+		$('#formularioEstilo').slideDown('slow', function() {
+  		});
+		
+		div.style.display = "none";
+		div3.style.display = "none";
+		div4.style.display = "none";
+
+	}
+
+	function mostrarCiudades() {
+
+		div = document.getElementById('resul');
+		div2 = document.getElementById('formularioEstilo');
+		div3 = document.getElementById('formularioCiudades');
+		div4 = document.getElementById('formularioCercania');
+		
+		$('#formularioCiudades').slideDown('slow', function() {
+ 		});
+		
+		div.style.display="none";
+		div2.style.display = "none";
+		div4.style.display = "none";
+
+	}
+		
+	function mostrarCercania() {
+
+		div = document.getElementById('resul');
+		div2 = document.getElementById('formularioEstilo');
+		div3 = document.getElementById('formularioCiudades');
+		div4 = document.getElementById('formularioCercania');
+		
+		$('#formularioCercania').slideDown('slow', function() {
+		});
+		
+		div.style.display="none";
+		div2.style.display = "none";
+		div3.style.display = "none";
+
+	}
+       
+    function obtenerDatos() {
+        
+        $.ajax({
+            url: 'http://192.168.1.102/evento_por_fecha.php',
+            dataType: 'jsonp',
+            jsonp: 'jsoncallback',
+            type:'get',
+            timeout: 5000,
+            success: function(data/*, status*/){
+                $.each(data, function(i,item){	              	  
+              	  	var celda = document.getElementById(i);
+              	  	celda.id=item.id;
+               	    celda.src='http://test.bandness.com/uploads/images/posters/'+item.picture;
+                });
+            },
+            error: function(){
+            }
+        });
+    }
+    
+    function continuar(){
+	    event.preventDefault();
+        linkLocation = "html/bandas.html";
+        $("body").fadeOut('slow',redirectPage);     
+	}	
+	
+	function zonaprivada(){
+		event.preventDefault();
+        linkLocation = "html/login.html";
+        $("body").fadeOut('slow',redirectPage);     
+	}
+	
+	function zonaeventos(i){
+		event.preventDefault();
+        linkLocation = "html/evento.html?Evento="+i;
+        $("body").fadeOut('slow',redirectPage);     
+	}
+	
+	function buscarCiudad(){
+		var tipo="CI";
+        valor=document.getElementById("Ciudad").value;
+        linkLocation = "html/bandas.html?Banda="+valor+"&tipo="+tipo;
+        $("body").fadeOut('slow',redirectPage);     	
+    }
+	
+	function buscarEstilo(){
+		var tipo="ES";
+        valor=document.getElementById("Estilo1").value;
+        estilo2=document.getElementById("Estilo2").value;
+        estilo3=document.getElementById("Estilo3").value;
+        linkLocation = "html/bandas.html?Banda="+valor+"&estilo2="+estilo2+"&estilo3="+estilo3+"&tipo="+tipo;	
+        $("body").fadeOut('slow',redirectPage);     
+    }
+    
+    function redirectPage() {
+		window.location = linkLocation;
+	}	
